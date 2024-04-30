323b025366f206c2b21281dda88ca0ae715da142
luyingjian <1425394610@qq.com>
2024-04-30 17:27:39 +0800

    创建vue3-ts项目

---
+++ shuhui-manage-vue3-ts/src/App.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import type { CSSProperties } from 'vue';
+const headerStyle: CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+};
+
+const contentStyle: CSSProperties = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+};
+
+const siderStyle: CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+};
+
+const footerStyle: CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+};
+</script>
+
+<template>
+  <div style="width: 100%;height: 100%;">
+    <a-layout>
+      <a-layout-header :style="headerStyle">Header</a-layout-header>
+      <a-layout>
+        <a-layout-sider :style="siderStyle">Sider</a-layout-sider>
+        <a-layout-content :style="contentStyle">Content</a-layout-content>
+      </a-layout>
+      <a-layout-footer :style="footerStyle">Footer</a-layout-footer>
+    </a-layout>
+  </div>
+
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
--- shuhui-manage-vue3/src/layout/index.vue
+++ shuhui-manage-vue3/src/layout/index.vue
@@ -1,45 +1,15 @@
 <template>
-  <a-layout>
-    <a-layout-header style>Header</a-layout-header>
     <a-layout>
-      <a-layout-sider style>Sider</a-layout-sider>
-      <a-layout-content style>Content</a-layout-content>
+      <a-layout-header >Header</a-layout-header>
+      <a-layout>
+        <a-layout-sider >Sider</a-layout-sider>
+        <a-layout-content >Content</a-layout-content>
+      </a-layout>
+      <a-layout-footer >Footer</a-layout-footer>
     </a-layout>
-    <a-layout-footer style>Footer</a-layout-footer>
-  </a-layout>
 </template>
 
 <script>
-import type { CSSProperties } from 'vue'
-const headerStyle: CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 64,
-  paddingInline: 50,
-  lineHeight: '64px',
-  backgroundColor: '#7dbcea'
-}
-
-const contentStyle: CSSProperties = {
-  textAlign: 'center',
-  minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#108ee9'
-}
-
-const siderStyle: CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#3ba0e9'
-}
-
-const footerStyle: CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#7dbcea'
-}
 </script>
 
 <style>
