a902dae3264b90eedd461c1ba49da11f56882520
luyingjian <1425394610@qq.com>
2024-05-11 16:03:11 +0800

    创建sql工具类controller

---
+++ shuhui-manage-java/shuhui-sys/src/main/java/cn/shuhui/modular/utils/controller/SqlController.java
@@ -0,0 +1,15 @@
+package cn.shuhui.modular.utils.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author luyingjian
+ * @date 2024/5/11
+ */
+@Api(tags = "sql工具接口")
+@RestController
+@RequestMapping("/sql")
+public class SqlController {
+}
